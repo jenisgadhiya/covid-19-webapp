dfff5fc2514fca14902d4ae6862fd3d8ed23df1a
jenis <jenugadhiya8@gmail.com>
2020-08-28 20:07:50 +0530

    correct speling of details..and add some details in about page

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ function getsname(){
             html="<h3>state doesnot exists...enter valid state<h3>"
             out.innerHTML=html;
         }else{
-            html="<h3>"+data.State+" state covid-19 case deatails...</h3>"
+            html="<h3>"+data.State+" state covid-19 case details...</h3>"
        
             html+="<h4 class='con'>State: "+data.State+"<br><br>Confirmed: "+data.Confirmed+"<br><br>Active: "+data.Active+"<br><br>Recovered: "+data.Recovered+"<br><br>Deaths: "+data.Death+"</h4>";
             out.innerHTML=html;
